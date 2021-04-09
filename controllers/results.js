5784aef01a85d00ca7f5ab23b551dc94103ee745
Emmanuella <emmavduru@gmail.com>
2021-04-09 09:18:19 +0100

    Fixed the back arrow error

--- controllers/results.js
+++ controllers/results.js
@@ -1,39 +1,40 @@
-const Candidate = require('../models/staff');
-const Exams = require('../models/exams');
-const Questions = require('../models/question');
-const Result = require('../models/results');
-const nodemailer = require('nodemailer');
+const Candidate = require("../models/staff");
+const Exams = require("../models/exams");
+const Questions = require("../models/question");
+const Result = require("../models/results");
+const nodemailer = require("nodemailer");
 const smtpTransport = require("nodemailer-smtp-transport");
 
-module.exports.getResponses = async(req, res)=>{
-
-    const candidate = req.user._id;
-    const {answers, questions, exam} = req.body;
-    const countOfQuestions = questions.length;
-    let score = 0;
-    let rightOptions = [];
-
+module.exports.getResponses = async (req, res) => {
+  const candidate = req.user._id;
+  const { answers, questions, exam } = req.body;
+  const countOfQuestions = questions.length;
+  let score = 0;
+  let rightOptions = [];
+  try {
     const candidatePhone = await Candidate.findById(candidate).select("phone");
 
-    const examNameQuery = await Exams.findById({_id:exam})
+    const examNameQuery = await Exams.findById({ _id: exam });
     // .populate("questions");
-    
-    for(question of questions){
-        const rightOption = await Questions.findById(question).select("correctAnswer");
-        const letter = rightOption.correctAnswer;
-        rightOptions.push(letter);
+
+    for (question of questions) {
+      const rightOption = await Questions.findById(question).select(
+        "correctAnswer"
+      );
+      const letter = rightOption.correctAnswer;
+      rightOptions.push(letter);
     }
 
-//function to count the number of matching options 
-    function compare(arr1,arr2){
-    let count=0;
-    const max=arr1.length>arr2.length ? arr2.length : arr1.length;
-    for(var i=0;i<max;i++){
-        if(arr1[i]==arr2[i]){
-        count++;
+    //function to count the number of matching options
+    function compare(arr1, arr2) {
+      let count = 0;
+      const max = arr1.length > arr2.length ? arr2.length : arr1.length;
+      for (var i = 0; i < max; i++) {
+        if (arr1[i] == arr2[i]) {
+          count++;
         }
-    }
-    return count;
+      }
+      return count;
     }
     //compare the users answers with the right options to get the count of correct answers
     const countOfMatchedAnswers = compare(answers, rightOptions);
@@ -41,70 +42,76 @@ module.exports.getResponses = async(req, res)=>{
     //calculate the students score
     score = Math.round((countOfMatchedAnswers / countOfQuestions) * 100) + "%";
 
-
     //convert users answers from array to a string to be saved to database
     const userChoices = answers.toString();
     const rightChoices = rightOptions.toString();
 
     //save the result
-    const newScore = new Result({score, candidate, phone:candidatePhone.phone, exam, userChoices, rightChoices, questions});
-
-    await newScore.save();
+    const newScore = await Result.create({
+      score,
+      candidate,
+      phone: candidatePhone.phone,
+      exam,
+      userChoices,
+      rightChoices,
+      questions,
+    });
 
     //send email to admin when students perform poorly on their exams
     //exclude scholarship exams to avoid spamming
 
-    if(examNameQuery.name.toLowerCase().includes("scholarship")=== false){
-        if(score.slice(0, score.indexOf('%')) < 50){
+    if (examNameQuery.name.toLowerCase().includes("scholarship") === false) {
+      if (score.slice(0, score.indexOf("%")) < 50) {
+        const transporter = nodemailer.createTransport(
+          smtpTransport({
+            service: "gmail",
+            host: "smtp.gmail.com",
+            auth: {
+              user: "loctechexamsengine@gmail.com",
+              pass: "locexams@pudopc6",
+            },
+          })
+        );
 
-            const transporter = nodemailer.createTransport(smtpTransport({
-                service: "gmail",
-                host: "smtp.gmail.com",
-                auth: {
-                    user: "loctechexamsengine@gmail.com",
-                    pass: "locexams@pudopc6"
-                }
-            }));
-        
-            const mailOptions = {
-                from: "loctechexamsengine@gmail.com",
-                to: "joy.okwu@loctech.ng, hope.israel@loctech.ng, promise.udo@loctech.ng",
-                subject:`Student Performance Report - ${req.user.username}`,
-                html: `<b>Dear Admin,</b><br><br>${req.user.username} has performed below average on ${examNameQuery.name} with a score of ${score}.<br><br>Best Regards,<br><i>Loctech Exams Engine App</i>`
-            }
-        
-            await transporter.sendMail(mailOptions, function(error, info){
-                if(error) console.log(error); else console.log("Email sent: ");
-                
-            });
-    
-        }
+        const mailOptions = {
+          from: "loctechexamsengine@gmail.com",
+          to:
+            "joy.okwu@loctech.ng, hope.israel@loctech.ng, promise.udo@loctech.ng",
+          subject: `Student Performance Report - ${req.user.username}`,
+          html: `<b>Dear Admin,</b><br><br>${req.user.username} has performed below average on ${examNameQuery.name} with a score of ${score}.<br><br>Best Regards,<br><i>Loctech Exams Engine App</i>`,
+        };
+
+        await transporter.sendMail(mailOptions, function (error, info) {
+          if (error) console.log(error);
+          else console.log("Email sent: ");
+        });
+      }
     }
- 
- };
-
-module.exports.renderResultIndex = async(req, res)=>{
-
-    const results = await Result.find()
-    .populate("candidate")
-    .populate("exam");
-
-    res.render('results/index', {results});
+  } catch (e) {
+    console.log(e);
+  }
 };
 
-module.exports.renderResultDetail = async(req, res)=>{
-    const {resultId} = req.params;
+module.exports.renderResultIndex = async (req, res) => {
+  const results = await Result.find().populate("candidate").populate("exam");
 
+  res.render("results/index", { results });
+};
 
-    const theResult = await Result.findById(resultId)
-        .populate("candidate")  
-        .populate("exam")
-        .populate("questions");
+module.exports.renderResultDetail = async (req, res) => {
+  const { resultId } = req.params;
 
+  const theResult = await Result.findById(resultId)
+    .populate("candidate")
+    .populate("exam")
+    .populate("questions");
 
-    let userChoicearr = theResult.userChoices.split(",");
-    let rightChoicearr = theResult.rightChoices.split(",");
- 
+  let userChoicearr = theResult.userChoices.split(",");
+  let rightChoicearr = theResult.rightChoices.split(",");
 
-    res.render('results/detail', {result:theResult, userChoicearr, rightChoicearr});
+  res.render("results/detail", {
+    result: theResult,
+    userChoicearr,
+    rightChoicearr,
+  });
 };
--- public/javascripts/running.js
+++ public/javascripts/running.js
@@ -1,10 +1,10 @@
-const queryString = window.location.pathname
+const queryString = window.location.pathname;
 // const urlParams = new URLSearchParams(queryString);
-const exam = queryString.slice(16,40);
+const exam = queryString.slice(16, 40);
 const examDuration = parseInt(document.querySelector("#duration").value);
-const nextQuestion = document.querySelector('.next-btn');
+const nextQuestion = document.querySelector(".next-btn");
 // console.log(questionId);
-const submitButton = document.querySelector(".submit-btn")
+const submitButton = document.querySelector(".submit-btn");
 //questions coming from Axios request
 const question = document.querySelector("#question");
 const answerBtns = document.querySelectorAll(".answer");
@@ -14,23 +14,23 @@ const optionA = document.querySelector("#optALabel");
 const optionB = document.querySelector("#optBLabel");
 const optionC = document.querySelector("#optCLabel");
 const optionD = document.querySelector("#optDLabel");
-const labelA = document.querySelector('#labelA');
-const labelB = document.querySelector('#labelB');
-const labelC = document.querySelector('#labelC');
-const labelD = document.querySelector('#labelD');
+const labelA = document.querySelector("#labelA");
+const labelB = document.querySelector("#labelB");
+const labelC = document.querySelector("#labelC");
+const labelD = document.querySelector("#labelD");
 const questionNum = document.querySelector("#question-num");
 const totalQuestionNum = document.querySelector("#total-question-num");
-const examTitle = document.querySelector('.exam-title');
-const questionBox = document.querySelector('.question-details');
-const r = Math.round((Math.random()*255) + 1);
-const g = Math.round((Math.random()*255) + 1);
-const b = Math.round((Math.random()*255) + 1);
+const examTitle = document.querySelector(".exam-title");
+const questionBox = document.querySelector(".question-details");
+const r = Math.round(Math.random() * 255 + 1);
+const g = Math.round(Math.random() * 255 + 1);
+const b = Math.round(Math.random() * 255 + 1);
 
-//change the exam-title border-top color 
+//change the exam-title border-top color
 examTitle.style.borderTopColor = `rgb(${r},${g},${b})`;
 questionBox.style.borderColor = `rgb(${r},${g},${b})`;
 
-for(navBtn of navBtns){
+for (navBtn of navBtns) {
   navBtn.style.backgroundColor = "red";
   navBtn.disabled = true;
 }
@@ -44,98 +44,95 @@ let index2 = 0;
 let count = 0;
 
 //Use axios to get the questions candidate is to answer
-axios.get("/candidate/getquestion", {
-  params: {
-    id: exam
-  }
-}).then((response)=>{
-if (response.status >= 400) {
-    console.log(response.status);
-    throw new Error("Bad response from server");
-} else{
-    return response.data
-}
-}).then((data)=>{
-  for(d of data){
-    //push all the questions fetched for the candidate into the questions array
+axios
+  .get("/candidate/getquestion", {
+    params: {
+      id: exam,
+    },
+  })
+  .then((response) => {
+    if (response.status >= 400) {
+      console.log(response.status);
+      throw new Error("Bad response from server");
+    } else {
+      return response.data;
+    }
+  })
+  .then((data) => {
+    for (d of data) {
+      //push all the questions fetched for the candidate into the questions array
       questions.push(d._id);
-  }
-  
-  //show the question when the route is changed
-  if(window.location.pathname.slice(-7)==="running"){
-    showQuestion(data, index);
+    }
 
-  }
+    //show the question when the route is changed
+    if (window.location.pathname.slice(-7) === "running") {
+      showQuestion(data, index);
+    }
 
-navBtns.forEach((navBtn)=>{
-    navBtn.addEventListener("click", (e)=>{
-        showQuestion(data, e.target.id-1);
+    navBtns.forEach((navBtn) => {
+      navBtn.addEventListener("click", (e) => {
+        showQuestion(data, e.target.id - 1);
         // console.log(e.target.id);
-        index2 = e.target.id-1;
-    })
-});
+        index2 = e.target.id - 1;
+      });
+    });
 
-navBtns.forEach((navBtn)=>{
-  navBtn.addEventListener("focus", (e)=>{
-      navBtn.style.backgroundColor= "orange";
-  })
-});
+    navBtns.forEach((navBtn) => {
+      navBtn.addEventListener("focus", (e) => {
+        navBtn.style.backgroundColor = "orange";
+      });
+    });
 
-navBtns.forEach((navBtn)=>{
-  navBtn.addEventListener("focusout", (e)=>{
-      navBtn.style.backgroundColor= "green";
-  })
-});
+    navBtns.forEach((navBtn) => {
+      navBtn.addEventListener("focusout", (e) => {
+        navBtn.style.backgroundColor = "green";
+      });
+    });
 
-
-
-
-  answerBtns.forEach((answerBtn) => {
-    answerBtn.addEventListener("click", (e) => {
-      //push candidate's answers into the answers array
+    answerBtns.forEach((answerBtn) => {
+      answerBtn.addEventListener("click", (e) => {
+        //push candidate's answers into the answers array
         // answers.push(e.target.id);
 
-      // console.log(index2);
+        // console.log(index2);
 
-      answers.splice(index2, 1, e.target.id);
-      console.log(index2, 'index 2')
-      turnGreen(index2);
-      disableNextBtn(index2);
-      console.log(answers);
-      // index++; //increment the index of the questions array
+        answers.splice(index2, 1, e.target.id);
+        console.log(index2, "index 2");
+        turnGreen(index2);
+        disableNextBtn(index2);
+        console.log(answers);
+        // index++; //increment the index of the questions array
 
-      //   // submit button submits
-      // if (index >= data.length) {
-      //   //when question is exhausted, send the answers to the backend
-      //   sendAnswers(answers, questions, exam);
-      // } else {
-      //   //if we've still got more questions then show them
+        //   // submit button submits
+        // if (index >= data.length) {
+        //   //when question is exhausted, send the answers to the backend
+        //   sendAnswers(answers, questions, exam);
+        // } else {
+        //   //if we've still got more questions then show them
 
-      //   // showQuestion(data, index);
-      // }
+        //   // showQuestion(data, index);
+        // }
+      });
+    });
+
+    submitButton.addEventListener("click", function () {
+      console.log(answers, "Final anwsers here");
+      sendAnswers(answers, questions, exam);
     });
   });
 
-  submitButton.addEventListener("click",function(){
-    console.log(answers, 'Final anwsers here')
-    sendAnswers(answers, questions, exam);
-
-  })
- 
-})
-
-function turnGreen(index){
-    let rightIndex = index+1;
-    let navBtn = document.getElementById(rightIndex);
-    console.log(navBtn);
-    navBtn.style.backgroundColor = "green";
+function turnGreen(index) {
+  let rightIndex = index + 1;
+  let navBtn = document.getElementById(rightIndex);
+  console.log(navBtn);
+  navBtn.style.backgroundColor = "green";
 }
 
-function disableNextBtn(index){
-  console.log(index+1);
+function disableNextBtn(index) {
+  console.log(index + 1);
   console.log(navBtns.length);
-  if(index+2 <= navBtns.length){
-    let rightIndex = index+2;
+  if (index + 2 <= navBtns.length) {
+    let rightIndex = index + 2;
     let navBtn = document.getElementById(rightIndex);
     navBtn.disabled = false;
   }
@@ -148,16 +145,15 @@ const showQuestion = (trivia, index) => {
     answerBtn.checked = false;
     answerBtn.setAttribute("name", triviaStr.name);
     console.log(answerBtn);
-   
-  })
+  });
 
-  console.log(index, 'index from show question');
+  console.log(index, "index from show question");
   let triviaOpt = answers[index];
 
-  if(triviaOpt != null){
+  if (triviaOpt != null) {
     document.getElementById(triviaOpt).checked = true;
   }
-  
+
   questionNum.textContent = index + 1;
   totalQuestionNum.textContent = trivia.length;
 
@@ -172,57 +168,48 @@ const showQuestion = (trivia, index) => {
   optionD.textContent = triviaStr.options.D;
 };
 
-
-
 //function to send the answers using the fetch API
-function sendAnswers(answers, questions, exam){
-
-    fetch("/candidate/exam/results", {
-        method: "POST",
-        body: JSON.stringify({ answers, questions, exam }),
-        headers: { "Content-Type": "application/json" },
-      });
-      window.location.pathname = '/candidate/thankyou'
+function sendAnswers(answers, questions, exam) {
+  fetch("/candidate/exam/results", {
+    method: "POST",
+    body: JSON.stringify({ answers, questions, exam }),
+    headers: { "Content-Type": "application/json" },
+  });
+  window.location.replace("/candidate/thankyou");
 }
-
 
 //this function starts the timer immediately we on the exams page
-if(queryString.slice(-7)==="running"){
+if (queryString.slice(-7) === "running") {
   // examDuration
-    countdown("exam-timer", examDuration , 0);
+  countdown("exam-timer", examDuration, 0);
 }
-
-
 
 //this function takes care of timing the candidate
 function countdown(elementName, minutes, seconds) {
-    var element, endTime, hours, mins, msLeft, time;
-  
-    function twoDigits(n) {
-      return n <= 9 ? "0" + n : n;
-    }
-  
-    function updateTimer() {
-      msLeft = endTime - +new Date();
-      if (msLeft < 1000) {
-        
-        document.querySelector('body').style.backgroundColor="red";
-        sendAnswers(answers, questions, exam);
-        window.location.pathname = '/candidate/thankyou'
+  var element, endTime, hours, mins, msLeft, time;
 
-      } else {
-        time = new Date(msLeft);
-        hours = time.getUTCHours();
-        mins = time.getUTCMinutes();
-        element.innerHTML =
-          (hours ? hours + ":" + twoDigits(mins) : mins) +
-          ":" +
-          twoDigits(time.getUTCSeconds());
-        setTimeout(updateTimer, time.getUTCMilliseconds() + 500);
-      }
-    }
-  
-    element = document.getElementById(elementName);
-    endTime = +new Date() + 1000 * (60 * minutes + seconds) + 500;
-    updateTimer();
+  function twoDigits(n) {
+    return n <= 9 ? "0" + n : n;
   }
+
+  function updateTimer() {
+    msLeft = endTime - +new Date();
+    if (msLeft < 1000) {
+      document.querySelector("body").style.backgroundColor = "red";
+      sendAnswers(answers, questions, exam);
+    } else {
+      time = new Date(msLeft);
+      hours = time.getUTCHours();
+      mins = time.getUTCMinutes();
+      element.innerHTML =
+        (hours ? hours + ":" + twoDigits(mins) : mins) +
+        ":" +
+        twoDigits(time.getUTCSeconds());
+      setTimeout(updateTimer, time.getUTCMilliseconds() + 500);
+    }
+  }
+
+  element = document.getElementById(elementName);
+  endTime = +new Date() + 1000 * (60 * minutes + seconds) + 500;
+  updateTimer();
+}

7c27f28fa4fd1fb4107c178919509f9c354547c0
Promise Udo <info.promiseudo@gmail.com>
2021-09-18 07:39:40 +0100

    add dist file for netlify

--- controllers/results.js
+++ controllers/results.js
@@ -2,8 +2,6 @@ const Candidate = require("../models/staff");
 const Exams = require("../models/exams");
 const Questions = require("../models/question");
 const Result = require("../models/results");
-const nodemailer = require("nodemailer");
-const smtpTransport = require("nodemailer-smtp-transport");
 
 module.exports.getResponses = async (req, res) => {
   const candidate = req.user._id;
@@ -47,7 +45,6 @@ module.exports.getResponses = async (req, res) => {
     const rightChoices = rightOptions.toString();
 
     //save the result
-     //save the result
      const newScore = await Result.create({
       score,
       candidate,
@@ -57,37 +54,6 @@ module.exports.getResponses = async (req, res) => {
       rightChoices,
       questions,
     });
-
-    //send email to admin when students perform poorly on their exams
-    //exclude scholarship exams to avoid spamming
-
-    if (examNameQuery.name.toLowerCase().includes("scholarship") === false) {
-      if (score.slice(0, score.indexOf("%")) < 50) {
-        const transporter = nodemailer.createTransport(
-          smtpTransport({
-            service: "gmail",
-            host: "smtp.gmail.com",
-            auth: {
-              user: "loctechexamsengine@gmail.com",
-              pass: "locexams@pudopc6",
-            },
-          })
-        );
-
-        const mailOptions = {
-          from: "loctechexamsengine@gmail.com",
-          to:
-            "joy.okwu@loctech.ng, hope.israel@loctech.ng, promise.udo@loctech.ng",
-          subject: `Student Performance Report - ${req.user.username}`,
-          html: `<b>Dear Admin,</b><br><br>${req.user.username} has performed below average on ${examNameQuery.name} with a score of ${score}.<br><br>Best Regards,<br><i>Loctech Exams Engine App</i>`,
-        };
-
-        await transporter.sendMail(mailOptions, function (error, info) {
-          if (error) console.log(error);
-          else console.log("Email sent: ");
-        });
-      }
-    }
   } catch (e) {
     console.log(e);
   }
@@ -95,6 +61,8 @@ module.exports.getResponses = async (req, res) => {
 
 module.exports.renderResultIndex = async (req, res) => {
   const results = await Result.find().populate("candidate").populate("exam");
+
+
 
   res.render("results/index", { results });
 };

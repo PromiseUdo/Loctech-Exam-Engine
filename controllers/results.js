54fa083382dcd8b940c4eee505189bac2420f69b
Promise Udo <info.promiseudo@gmail.com>
2021-09-21 09:29:22 +0100

    remove auto email feature for assessing students performance

--- controllers/results.js
+++ controllers/results.js
@@ -60,34 +60,6 @@ module.exports.getResponses = async (req, res) => {
 
     //send email to admin when students perform poorly on their exams
     //exclude scholarship exams to avoid spamming
-
-    if (examNameQuery.name.toLowerCase().includes("scholarship") === false) {
-      if (score.slice(0, score.indexOf("%")) < 50) {
-        const transporter = nodemailer.createTransport(
-          smtpTransport({
-            service: "gmail",
-            host: "smtp.gmail.com",
-            auth: {
-              user: "loctechexamsengine@gmail.com",
-              pass: "locexams@pudopc6",
-            },
-          })
-        );
-
-        const mailOptions = {
-          from: "loctechexamsengine@gmail.com",
-          to:
-            "joy.okwu@loctech.ng, hope.israel@loctech.ng, promise.udo@loctech.ng",
-          subject: `Student Performance Report - ${req.user.username}`,
-          html: `<b>Dear Admin,</b><br><br>${req.user.username} has performed below average on ${examNameQuery.name} with a score of ${score}.<br><br>Best Regards,<br><i>Loctech Exams Engine App</i>`,
-        };
-
-        await transporter.sendMail(mailOptions, function (error, info) {
-          if (error) console.log(error);
-          else console.log("Email sent: ");
-        });
-      }
-    }
   } catch (e) {
     console.log(e);
   }

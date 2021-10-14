29738138b3c2864fec90835990c6b6afde0da258
Promise Udo <info.promiseudo@gmail.com>
2021-10-14 13:28:47 +0100

    delete result when candidate has been deleted

--- models/staff.js
+++ models/staff.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const passportLocalMongoose = require('passport-local-mongoose');
 const Schema = mongoose.Schema;
+const Results = require('./result');
 
 
 //create a staff schema
@@ -19,11 +20,26 @@ const userSchema = new Schema({
     },
     role:{
         type:String  
+    },
+    results:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref: 'Result'
     }
 });
 
 //will add on fields for username and password, makes them unique and give us additional methods to use
 userSchema.plugin(passportLocalMongoose);
+
+
+userSchema.post('findOneAndDelete', async function(doc){
+    if(doc){
+        await Results.deleteMany({
+            _id:{$in:doc.results}
+        })
+    }else{
+    
+    }
+});
 
 
 //compile and export
